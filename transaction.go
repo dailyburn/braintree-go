601837ba8c5c3483d82ddef2fd55c1177837ea12
Ryan Kulla <rkulla@gmail.com>
2016-09-28 10:00:02 -0700

    Adds status-history support

--- transaction.go
+++ transaction.go
@@ -7,38 +7,51 @@ import (
 )
 
 type Transaction struct {
-	XMLName                    string               `xml:"transaction"`
-	Id                         string               `xml:"id,omitempty"`
-	CustomerID                 string               `xml:"customer-id,omitempty"`
-	Status                     string               `xml:"status,omitempty"`
-	Type                       string               `xml:"type,omitempty"`
-	Amount                     *Decimal             `xml:"amount"`
-	OrderId                    string               `xml:"order-id,omitempty"`
-	PaymentMethodToken         string               `xml:"payment-method-token,omitempty"`
-	PaymentMethodNonce         string               `xml:"payment-method-nonce,omitempty"`
-	MerchantAccountId          string               `xml:"merchant-account-id,omitempty"`
-	PlanId                     string               `xml:"plan-id,omitempty"`
-	CreditCard                 *CreditCard          `xml:"credit-card,omitempty"`
-	Customer                   *Customer            `xml:"customer,omitempty"`
-	BillingAddress             *Address             `xml:"billing,omitempty"`
-	ShippingAddress            *Address             `xml:"shipping,omitempty"`
-	DeviceData                 string               `xml:"device-data,omitempty"`
-	Options                    *TransactionOptions  `xml:"options,omitempty"`
-	ServiceFeeAmount           *Decimal             `xml:"service-fee-amount,attr,omitempty"`
-	CreatedAt                  *time.Time           `xml:"created-at,omitempty"`
-	UpdatedAt                  *time.Time           `xml:"updated-at,omitempty"`
-	DisbursementDetails        *DisbursementDetails `xml:"disbursement-details,omitempty"`
-	RefundId                   string               `xml:"refund-id,omitempty"`
-	RefundIds                  *[]string            `xml:"refund-ids>item,omitempty"`
-	RefundedTransactionId      *string              `xml:"refunded-transaction-id,omitempty"`
-	ProcessorResponseCode      int                  `xml:"processor-response-code,omitempty"`
-	ProcessorResponseText      string               `xml:"processor-response-text,omitempty"`
-	ProcessorAuthorizationCode string               `xml:"processor-authorization-code,omitempty"`
-	SettlementBatchId          string               `xml:"settlement-batch-id,omitempty"`
-	Subscription               *Subscription        `xml:"subscription,omitempty"`
-	AddOns                     *AddOnResults        `xml:"add-ons,omitempty"`
-	Discounts                  *DiscountResults     `xml:"discounts,omitempty"`
-	TaxAmount                  string               `xml:"tax-amount,omitempty"`
+	XMLName                    string                `xml:"transaction"`
+	Id                         string                `xml:"id,omitempty"`
+	CustomerID                 string                `xml:"customer-id,omitempty"`
+	Status                     string                `xml:"status,omitempty"`
+	Type                       string                `xml:"type,omitempty"`
+	Amount                     *Decimal              `xml:"amount"`
+	OrderId                    string                `xml:"order-id,omitempty"`
+	PaymentMethodToken         string                `xml:"payment-method-token,omitempty"`
+	PaymentMethodNonce         string                `xml:"payment-method-nonce,omitempty"`
+	MerchantAccountId          string                `xml:"merchant-account-id,omitempty"`
+	PlanId                     string                `xml:"plan-id,omitempty"`
+	CreditCard                 *CreditCard           `xml:"credit-card,omitempty"`
+	Customer                   *Customer             `xml:"customer,omitempty"`
+	BillingAddress             *Address              `xml:"billing,omitempty"`
+	ShippingAddress            *Address              `xml:"shipping,omitempty"`
+	DeviceData                 string                `xml:"device-data,omitempty"`
+	Options                    *TransactionOptions   `xml:"options,omitempty"`
+	ServiceFeeAmount           *Decimal              `xml:"service-fee-amount,attr,omitempty"`
+	CreatedAt                  *time.Time            `xml:"created-at,omitempty"`
+	UpdatedAt                  *time.Time            `xml:"updated-at,omitempty"`
+	DisbursementDetails        *DisbursementDetails  `xml:"disbursement-details,omitempty"`
+	RefundId                   string                `xml:"refund-id,omitempty"`
+	RefundIds                  *[]string             `xml:"refund-ids>item,omitempty"`
+	RefundedTransactionId      *string               `xml:"refunded-transaction-id,omitempty"`
+	ProcessorResponseCode      int                   `xml:"processor-response-code,omitempty"`
+	ProcessorResponseText      string                `xml:"processor-response-text,omitempty"`
+	ProcessorAuthorizationCode string                `xml:"processor-authorization-code,omitempty"`
+	SettlementBatchId          string                `xml:"settlement-batch-id,omitempty"`
+	Subscription               *Subscription         `xml:"subscription,omitempty"`
+	AddOns                     *AddOnResults         `xml:"add-ons,omitempty"`
+	Discounts                  *DiscountResults      `xml:"discounts,omitempty"`
+	TaxAmount                  string                `xml:"tax-amount,omitempty"`
+	StatusHistory              *StatusHistoryResults `xml:"status-history,omitempty"`
+}
+
+type StatusEventItem struct {
+	Timestamp         *time.Time `xml:"timestamp,omitempty"`
+	Status            string     `xml:"status,omitempty"`
+	Amount            *Decimal   `xml:"amount,omitempty"`
+	User              string     `xml:"user,omitempty"`
+	TransactionSource string     `xml:"transaction-source,omitempty"`
+}
+
+type StatusHistoryResults struct {
+	StatusEvent []StatusEventItem `xml:"status-event,omitempty"`
 }
 
 // TODO: not all transaction fields are implemented yet, here are the missing fields (add on demand)
@@ -55,29 +68,6 @@ type Transaction struct {
 //   <voice-referral-number nil="true"></voice-referral-number>
 //   <purchase-order-number nil="true"></purchase-order-number>
 //   <tax-exempt type="boolean">false</tax-exempt>
-//   <status-history type="array">
-//     <status-event>
-//       <timestamp type="datetime">2013-10-07T17:26:14Z</timestamp>
-//       <status>authorized</status>
-//       <amount>7.00</amount>
-//       <user>eaigner</user>
-//       <transaction-source>Recurring</transaction-source>
-//     </status-event>
-//     <status-event>
-//       <timestamp type="datetime">2013-10-07T17:26:14Z</timestamp>
-//       <status>submitted_for_settlement</status>
-//       <amount>7.00</amount>
-//       <user>eaigner</user>
-//       <transaction-source>Recurring</transaction-source>
-//     </status-event>
-//     <status-event>
-//       <timestamp type="datetime">2013-10-08T07:06:38Z</timestamp>
-//       <status>settled</status>
-//       <amount>7.00</amount>
-//       <user nil="true"></user>
-//       <transaction-source></transaction-source>
-//     </status-event>
-//   </status-history>
 //   <plan-id>bronze</plan-id>
 //   <subscription-id>jqsydb</subscription-id>
 //   <descriptor>
